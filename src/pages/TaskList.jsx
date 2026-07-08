69a2b17324a04fb4dbed4c0573c06e73a9cb3836
NicoSciangula <nico.sciangula99@gmail.com>
2026-07-08 12:19:41 +0200

    aggiunta funzione di debounce e la ricerca delle task

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,14 +1,29 @@
 import TaskRow from "../components/TaskRow";
 import { GlobalContext } from "../context/GlobalContext";
-import { useContext, useMemo, useState } from "react";
+import { useCallback, useContext, useMemo, useState } from "react";
+
+//* Funzione di debounds
+function debounce(callback, delay) {
+  let timer;
+  return (value) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback(value);
+    }, delay);
+  };
+}
 
 export default function TaskList() {
   const { tasks } = useContext(GlobalContext);
   const [sortBy, setSortBy] = useState("createdAt");
   const [sortOrder, setSortOrder] = useState(1);
+  const [search, setSearch] = useState("");
 
   const arrowIcon = sortOrder === 1 ? "▲" : "▼";
 
+  const debounceSearch = useCallback(debounce(setSearch, 500), []);
+
+  //* Ordinamento delle task
   const handleSortBy = (field) => {
     if (sortBy === field) {
       setSortOrder((prev) => prev * -1);
@@ -17,7 +32,6 @@ export default function TaskList() {
       setSortOrder(1);
     }
   };
-
   const sortedTask = useMemo(() => {
     return [...tasks].sort((a, b) => {
       let compare;
@@ -33,9 +47,26 @@ export default function TaskList() {
     });
   }, [tasks, sortBy, sortOrder]);
 
+  //* Filtraggio task
+  const filteredTask = useMemo(() => {
+    if (search === "") return sortedTask;
+    return sortedTask.filter((t) => {
+      return t.title.toLowerCase().includes(search.toLowerCase());
+    });
+  }, [search, sortedTask]);
+
   return (
     <>
       <main className="d-flex flex-column align-items-center">
+        <div className="d-flex justify-content-center align-items-center mt-3">
+          <input
+            className="form-control me-2"
+            type="text"
+            placeholder="Cerca task..."
+            onChange={(e) => debounceSearch(e.target.value)}
+          />
+          <i className="bi bi-search fs-4 text-secondary"></i>
+        </div>
         <div className="mt-3 table-responsive border rounded overflow-hidden container-table-task">
           <table className="table table-light table-hover text-center mb-0">
             <thead className="thead-bg">
@@ -53,7 +84,7 @@ export default function TaskList() {
               </tr>
             </thead>
             <tbody>
-              {sortedTask.map((t) => (
+              {filteredTask.map((t) => (
                 <TaskRow key={t.id} tasks={t} />
               ))}
             </tbody>
